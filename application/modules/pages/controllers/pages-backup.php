59e15827491a052a4553304ddef44f9f6d6b2f02
Syahroni Wahyu <roniwahyu@gmail.com>
2013-10-24 12:44:02 +0700

    integrasi ckeditor dengan cms
    coba integrasi ckeditor ke cms untuk mengelola page

---
+++ application/modules/pages/controllers/pages-backup.php
@@ -0,0 +1,158 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class pages extends MX_Controller {
+    const IMAGE_UPLOAD_DIR = 'assets/uploads/images';
+    public $data    =   array();
+
+    public function __construct() {
+        parent::__construct();
+        if ($this->session->userdata("login") != TRUE) {
+            $this->session->set_flashdata('login_notif','<p>You must be logged in</p>');
+            //redirect('login', 'refresh');
+        }
+        //Load IgnitedDatatables Library
+        $this->load->library('Datatables');
+        $this->load->model('pagesadmin_model','pagesdb',TRUE);
+        // $this->load->helper('url'); //You should autoload this one ;)
+        $this->load->helper('ckeditor');
+        
+        $this->_supported_extensions = array('jpg', 'jpeg', 'gif', 'png', 'pdf');
+ 
+ 
+        //Ckeditor's configuration
+        $data['ckeditor'] = array(
+ 
+            //ID of the textarea that will be replaced
+            'id'    =>  'content',
+            'path'  =>  'assets/ckeditor',
+ 
+            //Optionnal values
+            'config' => array(
+                'toolbar'   =>  "Full",     //Using the Full toolbar
+                'width'     =>  "550px",    //Setting a custom width
+                'height'    =>  '100px',    //Setting a custom height
+ 
+            ),
+ 
+            //Replacing styles from the "Styles tool"
+            'styles' => array(
+ 
+                //Creating a new style named "style 1"
+                'style 1' => array (
+                    'name'      =>  'Blue Title',
+                    'element'   =>  'h2',
+                    'styles' => array(
+                        'color'     =>  'Blue',
+                        'font-weight'   =>  'bold'
+                    )
+                ),
+ 
+                //Creating a new style named "style 2"
+                'style 2' => array (
+                    'name'  =>  'Red Title',
+                    'element'   =>  'h2',
+                    'styles' => array(
+                        'color'         =>  'Red',
+                        'font-weight'       =>  'bold',
+                        'text-decoration'   =>  'underline'
+                    )
+                )               
+            )
+        );
+ 
+        $data['ckeditor_2'] = array(
+ 
+            //ID of the textarea that will be replaced
+            'id'    =>  'content_2',
+            'path'  =>  'assets/ckeditor',
+ 
+            //Optionnal values
+            'config' => array(
+                'width'     =>  "550px",    //Setting a custom width
+                'height'    =>  '100px',    //Setting a custom height
+                'toolbar'   =>  array(  //Setting a custom toolbar
+                    array('Bold', 'Italic'),
+                    array('Underline', 'Strike', 'FontSize'),
+                    array('Smiley'),
+                    '/'
+                )
+            ),
+ 
+            //Replacing styles from the "Styles tool"
+            'styles' => array(
+ 
+                //Creating a new style named "style 1"
+                'style 3' => array (
+                    'name'      =>  'Green Title',
+                    'element'   =>  'h3',
+                    'styles' => array(
+                        'color'     =>  'Green',
+                        'font-weight'   =>  'bold'
+                    )
+                )
+ 
+            )
+        );   
+        // $this->load->helper('tinymce');
+    }
+
+    public function index() {
+        $data['option_parent']=$this->pagesdb->get_dropdown_array('id','title','pages');
+       
+        $this->load->view('pages_view',$data);
+    }
+     
+
+    public function getdatatables(){
+        $this->datatables->select('id,title,body,slug,parent_id,template')
+                        ->from('pages');
+        echo $this->datatables->generate();
+    }
+
+   
+
+    public function get($id=null){
+        if($id!==null){
+            echo json_encode($this->pagesdb->get_one($id));
+        }
+    }
+
+    public function submit(){
+        if ($this->input->post('ajax')){
+          if ($this->input->post('id')){
+            $this->pagesdb->update($this->input->post('id'));
+          }else{
+            $this->pagesdb->save();
+          }
+
+        }else{
+          if ($this->input->post('submit')){
+              if ($this->input->post('id')){
+                $this->pagesdb->update($this->input->post('id'));
+              }else{
+                $this->pagesdb->save();
+              }
+          }
+        }
+    }
+
+    public function delete($id=null) {
+      $id = $this->input->post('id');
+        if (isset($_POST['id'])) {
+            $id=$_POST['id'];
+            $this->db->delete('pages', array('id' => $id));
+            $this->session->set_flashdata('notif','data has been deleted');
+        } elseif(!empty($id)){
+            $hapus['kode'] = $this->uri->segment(3);
+            $this->db->delete('pages', array('id' => $hapus['kode']));
+        } else {
+            $this->session->set_flashdata('notif', 'no data deleted');
+        }
+    }
+        
+ 
+    
+}
+
+/** Module pages Controller **/
+/** Build & Development By Syahroni Wahyu - roniwahyu@gmail.com */
